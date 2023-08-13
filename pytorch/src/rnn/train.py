0b059a400618f94d1151c04e6fdd21d996061d37
carrieyin <695685009@qq.com>
2023-08-13 16:30:12 +0800

    train lstm

--- pytorch/src/rnn/train.py
+++ pytorch/src/rnn/train.py
@@ -43,40 +43,6 @@ def train(epoch, imdb_model, lr, train_batch_size):
                                                                       acc))
             torch.save(imdb_model.state_dict(), '../../resources/model_save/imdb_net.pkl')
             torch.save(optimizer.state_dict(), '../../resources/model_save/imdb_optimizer.pkl')
-# def _get_batch(batch, ctx):
-#     """Return features and labels on ctx."""
-#     features, labels = batch
-#     if labels.dtype != features.dtype:
-#         labels = labels.astype(features.dtype)
-#     return (gutils.split_and_load(features, ctx),
-#             gutils.split_and_load(labels, ctx), features.shape[0])
-
-# def train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs):
-#     """Train and evaluate a model."""
-#     print('training on', ctx)
-#     if isinstance(ctx, mx.Context):
-#         ctx = [ctx]
-#     for epoch in range(num_epochs):
-#         train_l_sum, train_acc_sum, n, m, start = 0.0, 0.0, 0, 0, time.time()
-#         for i, batch in enumerate(train_iter):
-#             Xs, ys, batch_size = _get_batch(batch, ctx)
-#             with autograd.record():
-#                 y_hats = [net(X) for X in Xs]
-#                 ls = [loss(y_hat, y) for y_hat, y in zip(y_hats, ys)]
-#             for l in ls:
-#                 l.backward()
-#             trainer.step(batch_size)
-#             train_l_sum += sum([l.sum().asscalar() for l in ls])
-#             n += sum([l.size for l in ls])
-#             train_acc_sum += sum([(y_hat.argmax(axis=1) == y).sum().asscalar()
-#                                  for y_hat, y in zip(y_hats, ys)])
-#             m += sum([y.size for y in ys])
-#         test_acc = evaluate_accuracy(test_iter, net, ctx)
-#         print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, '
-#               'time %.1f sec'
-#               % (epoch + 1, train_l_sum / n, train_acc_sum / m, test_acc,
-#                  time.time() - start))
-
 
 def test(imdb_model, test_batch_size):
     imdb_model.eval()
