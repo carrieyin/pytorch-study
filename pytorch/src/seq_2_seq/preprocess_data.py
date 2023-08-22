ca2f3149b98d9ff5ebfbea6baf475913125510f1
carrieyin <695685009@qq.com>
2023-08-22 17:18:08 +0800

    se

--- pytorch/src/seq_2_seq/preprocess_data.py
+++ pytorch/src/seq_2_seq/preprocess_data.py
@@ -17,9 +17,9 @@ def download_extract():
         return extract_content(file_path)
 
     data_url = "http://www.manythings.org/anki/fra-eng.zip"
-    zip_file = requests.get(data_url)
-    with open(file_path, 'a') as f:
+    zip_path = r"../../../dataset/nmt/frg-eng.zip"
+    zip_file = requests.get(data_url, stream=True, verify=True)
+    with open(zip_path, 'wb') as f:
         f.write(zip_file.content)
-        f.close()
 def preprocess():
     pass
