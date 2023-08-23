38bff004ee1f7fb62f0ccf62e9f3dbabf798f56b
carrieyin <695685009@qq.com>
2023-08-23 17:18:44 +0800

    add nmt_download

--- pytorch/test/seq2seq/preprocess_test.py
+++ pytorch/test/seq2seq/preprocess_test.py
@@ -1,4 +1,11 @@
-from pytorch.src.seq_2_seq.preprocess_data import download_extract
+from pytorch.src.seq_2_seq.config import raw_file_path, raw_zip_path
+from pytorch.src.seq_2_seq.preprocess_data import download_extract, extract_content, preprocess_nmt, tokenize_nmt
+
+
 
 if __name__ == '__main__':
-    download_extract()
+    content = extract_content()
+    data = preprocess_nmt(content)
+    #print(data)
+    token_data = tokenize_nmt(data, 600)
+    print(token_data)
