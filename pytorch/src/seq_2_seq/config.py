2b9c54425b32f6a6a93f953f02b97b0407837b2c
carrieyin <695685009@qq.com>
2023-08-27 22:14:35 +0800

    seq

--- pytorch/src/seq_2_seq/config.py
+++ pytorch/src/seq_2_seq/config.py
@@ -5,12 +5,11 @@ import torch
 raw_file_path = r"..\..\..\dataset\nmt\fra.txt"
 raw_zip_path = r"..\..\..\dataset\nmt\fra-eng.zip"
 URL = "http://www.manythings.org/anki/fra-eng.zip"
-num_layers, hidden_size, batch_size, num_steps = 2, 300, 64, 10
+num_layers, hidden_size, batch_size, num_steps = 2, 300, 128, 10
 embed_size = 100
 num_examples = 800
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-print(torch.cuda.is_available())
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 #print(torch.cuda.current_device())
--- pytorch/test/seq2seq/predict_test.py
+++ pytorch/test/seq2seq/predict_test.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 
 from pytorch.src.seq_2_seq import config
@@ -9,18 +11,25 @@ from pytorch.src.seq_2_seq.predict_seq2seq import predict_seq2seq
 from pytorch.src.seq_2_seq.preprocess_data import load_data_nmt
 from pytorch.src.seq_2_seq.train import train
 
-data_iter, src_vocab, tgt_vocab = load_data_nmt(config.batch_size, config.num_steps, num_examples=600)
+start = time.time()
+data_iter, src_vocab, tgt_vocab = load_data_nmt(config.batch_size, config.num_steps, num_examples=60000)
 
+print("load data end")
 encoder = Encoder(len(src_vocab), config.embed_size, config.hidden_size, config.num_layers)
 decoder = Decoder(len(tgt_vocab), config.embed_size, config.hidden_size, config.num_layers)
 net = EncoderDecoder(encoder, decoder)
 
 lr, num_epochs = 0.01, 5
+print("traing start")
 train(net, data_iter, lr, num_epochs, tgt_vocab, device)
+print("traing end")
 
-print('predict')
+print('predict start')
 fras = ['va !', 'j\'ai perdu .', 'il est calme .', 'On ne vous a pas vu depuis longtemps .']
 engs = ['go .', "i lost .", 'he\'s calm .', 'We haven\'t seen you in a while .']
 for fra in fras:
     output_setence, _ = predict_seq2seq(net, fra, src_vocab, tgt_vocab, config.num_steps, device)
     print('{} = > {}'.format(fra, output_setence))
+
+end = time.time()
+print("运行时间为：", end - start, '秒')
